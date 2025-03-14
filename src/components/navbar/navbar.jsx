2dd945a0d65860cf792cfed2cfd99ab8397cebd5
QwakuMalone <ofoefrancisteye@gmail.com>
2025-03-14 02:40:06 +0000

    update nav

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -93,7 +93,7 @@ export default function Navbar({ isDarkMode, setIsDarkMode }) {
                     <button onClick={() => { setIsDarkMode(!isDarkMode) }} className="size-8">
                         <motion.svg
                             animate={isDarkMode ? { rotate: 180 } : {}}
-                            xmlns="http://www.w3.org/2000/svg" className="size-8 " viewBox="0 0 24 24"><path d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12-5.373-12-12-12-12 5.373-12 12zm2 0c0-5.514 4.486-10 10-10v20c-5.514 0-10-4.486-10-10z" /></motion.svg>
+                            xmlns="http://www.w3.org/2000/svg" className="size-8 dark:fill-white " viewBox="0 0 24 24"><path d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12-5.373-12-12-12-12 5.373-12 12zm2 0c0-5.514 4.486-10 10-10v20c-5.514 0-10-4.486-10-10z" /></motion.svg>
                     </button>
 
 
